7206caf8e457423cb7aaa587fe8cd83d5ece0362
Filipe Straubel <fstraubel10@gmail.com>
2025-03-26 09:35:56 -0300

    Feito a estrutura base do codigo (entrada de dados, validação de numero e desenho do diamante) totalmente funcional

--- TrabalhoDiamanteDeX.ConsoleApp/Program.cs
+++ TrabalhoDiamanteDeX.ConsoleApp/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            while (true)
+            {
+
+                Console.Write("Digite um numero impar: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("Digite um numero valido");
+                    continue;
+                }
+
+                int meio = numero / 2;
+                
+                for (int linhas = 0; linhas <= meio; linhas++) 
+                {
+                    
+                    Console.Write(new string(' ', meio - linhas));
+                    Console.WriteLine(new string('X', 2 * linhas + 1));
+                    
+                }
+
+                for (int linhas = meio - 1; linhas >=0; linhas--)
+                {
+                    Console.Write(new string(' ', meio - linhas));
+                    Console.WriteLine(new string('X', 2 * linhas + 1));
+                    
+                }
+
+
+                Console.WriteLine("Deseja Continuar? ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+                if (opcaoContinuar != "S")
+                    break;
+
+                Console.ReadLine();
+
+            }
+
         }
     }
 }
